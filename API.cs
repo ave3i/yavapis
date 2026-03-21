fe2e0ed8dcefa3d0616346bb406b47bab2fda5ab
avei <aveeIsPro@gmail.com>
2026-03-22 01:40:01 +0200

    Create API.cs

---
+++ API.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yavAPI
+{
+    public class yavFUNC
+    {
+        public const string DllName = "yav-module.dll";
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void Attach();
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+        public static extern void Execute([MarshalAs(UnmanagedType.LPWStr)] string input);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        [return: MarshalAs(UnmanagedType.I1)]
+        public static extern bool IsAttached();
+    }
+}
